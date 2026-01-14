9298f122d379e4947ec612e5aa0032a7fe45b8b6
Justin <justinpretorius007@gmail.com>
2026-01-14 16:51:08 +0200

    Add yfinance to requirements

--- web_app.py
+++ web_app.py
@@ -49,6 +49,8 @@ def home():
 def api_signal():
     return jsonify(get_signal())
 
+import os
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
